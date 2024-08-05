e0669cce6e2fec16171a458dde5311a1b77514c2
Documenter.jl <documenter@juliadocs.github.io>
2024-08-05 12:13:40 +0000

    build based on 6a52067

--- dev/assets/api.md.Bxc771Ca.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c as o,o as i,a7 as s}from"./chunks/framework.Cv41BB_P.js";const b=JSON.parse('{"title":"API","description":"","frontmatter":{},"headers":[],"relativePath":"api.md","filePath":"api.md","lastUpdated":null}'),a={name:"api.md"},t=s("",849),d=[t];function r(n,l,c,p,h,k){return i(),o("div",null,d)}const u=e(a,[["render",r]]);export{b as __pageData,u as default};
